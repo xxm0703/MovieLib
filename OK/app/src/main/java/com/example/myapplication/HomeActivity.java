28b3451074d996cf6092ac5d777a1b5736864515
Teddy15 <t.k.yovcheva@gmail.com>
2019-05-26 16:47:42 +0300

    Added Navigation Menu

--- OK/app/src/main/java/com/example/myapplication/HomeActivity.java
+++ OK/app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -2,21 +2,43 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
     Users db;
-    Button mButtonUpdate;
-    Button mButtonDelete;
+   // Button mButtonUpdate;
+    //Button mButtonDelete;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         db = new Users(this);
-        mButtonUpdate = (Button) findViewById(R.id.button_update);
+
+        NavigationView nav_view = (NavigationView) findViewById(R.id.nav_view);
+
+        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id =  menuItem.getItemId();
+                if(id == R.id.nav_view_change) {
+                    Intent changeInformation = new Intent(HomeActivity.this, ChangePasswordActivity.class);
+                    startActivity(changeInformation);
+                }
+                if(id == R.id.nav_view_delete) {
+                    Intent toLogin = new Intent(HomeActivity.this, DeleteAccountActivity.class);
+                    startActivity(toLogin);
+                }
+                return true;
+            }
+        });
+
+        /*mButtonUpdate = (Button) findViewById(R.id.button_update);
         mButtonDelete = (Button) findViewById(R.id.button_delete);
 
         mButtonUpdate.setOnClickListener(new View.OnClickListener() {
@@ -33,7 +55,7 @@ public class HomeActivity extends AppCompatActivity {
                 Intent toLogin = new Intent(HomeActivity.this, DeleteAccountActivity.class);
                 startActivity(toLogin);
             }
-        });
+        });*/
 
     }
 }
